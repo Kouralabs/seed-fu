adceccbf2782d74c5757427fa02582dd8934f99d
Tatu Argillander <tatu.argillander@kouralabs.com>
2020-08-25 16:34:39 +0300

    test

--- spec/connections/mysql.rb
+++ spec/connections/mysql.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
 ActiveRecord::Base.establish_connection(
-  :adapter  => "mysql2",
-  :database => "seed_fu_test",
-  :username => "root"
+  adapter: 'mysql2',
+  database: ENV.fetch('MYSQL_DATABASE', 'seed_fu_test'),
+  host: ENV.fetch('MYSQL_HOST', 'localhost'),
+  username: ENV.fetch('MYSQL_USER', 'postgres'),
+  password: ENV.fetch('MYSQL_PASSWORD', 'postgres'),
+  post: ENV.fetch('MYSQL_PORT', 3306)
 )
