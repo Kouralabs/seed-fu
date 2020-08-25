ab31ee1dcefefc59c6b22130150e440f7fa2ed5e
Tatu Argillander <tatu.argillander@kouralabs.com>
2020-08-25 16:37:55 +0300

    fix host

--- spec/connections/mysql.rb
+++ spec/connections/mysql.rb
@@ -3,7 +3,7 @@
 ActiveRecord::Base.establish_connection(
   adapter: 'mysql2',
   database: ENV.fetch('MYSQL_DATABASE', 'seed_fu_test'),
-  host: ENV.fetch('MYSQL_HOST', 'localhost'),
+  host: ENV.fetch('MYSQL_HOST', '127.0.0.1'),
   username: ENV.fetch('MYSQL_USER', 'postgres'),
   password: ENV.fetch('MYSQL_PASSWORD', 'postgres'),
   post: ENV.fetch('MYSQL_PORT', 3306)
