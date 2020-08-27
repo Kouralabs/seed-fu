3a34fe11cbdc05c84981879f18cc07770f4bb883
Tatu Argillander <tatu.argillander@kouralabs.com>
2020-08-27 12:36:53 +0300

    fix callback format

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rubygems'
 require 'bundler/setup'
 require 'seed-fu'
@@ -5,45 +7,59 @@ require 'logger'
 
 SeedFu.quiet = true
 
-ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + "/../debug.log")
+ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + '/../debug.log')
 
-ENV["DB"] ||= 'sqlite3'
-puts "Using #{ENV["DB"]} to run the tests."
-require File.dirname(__FILE__) + "/connections/#{ENV["DB"]}.rb"
+ENV['DB'] ||= 'sqlite3'
+puts "Using #{ENV['DB']} to run the tests."
+require File.dirname(__FILE__) + "/connections/#{ENV['DB']}.rb"
 
-ActiveRecord::Schema.define :version => 0 do
-  create_table :seeded_models, :force => true do |t|
+ActiveRecord::Schema.define version: 0 do
+  create_table :seeded_models, force: true do |t|
     t.column :login, :string
     t.column :first_name, :string
     t.column :last_name, :string
     t.column :title, :string
   end
 
-  create_table :seeded_model_no_primary_keys, :id => false, :force => true do |t|
+  create_table :seeded_model_no_primary_keys, id: false, force: true do |t|
     t.column :id, :string
   end
 
-  create_table :seeded_model_no_sequences, :id => false, :force => true do |t|
+  create_table :seeded_model_no_sequences, id: false, force: true do |t|
     t.column :id, :string
   end
 
-  execute("ALTER TABLE seeded_model_no_sequences ADD PRIMARY KEY (id)") if ENV['DB'] == 'postgresql'
+  execute('ALTER TABLE seeded_model_no_sequences ADD PRIMARY KEY (id)') if ENV['DB'] == 'postgresql'
 end
+
+def wrap_ar_cb; end
 
 class SeededModel < ActiveRecord::Base
   validates_presence_of :title
-  attr_protected :first_name if self.respond_to?(:protected_attributes)
+  attr_protected :first_name if respond_to?(:protected_attributes)
   attr_accessor :fail_to_save
 
-  before_save { false if fail_to_save }
+  before_save do
+    # Older versions of Rails allowed ActiveRecord callbacks to
+    # fail based on the return value. Official documentation states
+    # that this was changed in 5.0.0,
+    # https://apidock.com/rails/v5.0.0.1/ActiveRecord/Callbacks. But
+    # there's a bug in the documentation, this didn't happen before 5.1.0.
+    # Unless there was some minor release in between which introduced this.
+    last_supports_return_version = Gem::Version.new('5.0.0')
+
+    if ActiveRecord.version > last_supports_return_version
+      raise ActiveRecord::RecordNotSaved, ':fail_to_save' if fail_to_save
+    end
+
+    false if fail_to_save
+  end
 end
 
 class SeededModelNoPrimaryKey < ActiveRecord::Base
-
 end
 
 class SeededModelNoSequence < ActiveRecord::Base
-
 end
 
 RSpec.configure do |config|
